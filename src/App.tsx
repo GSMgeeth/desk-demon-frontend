56c7423351476b1847d9049cc1d2b523b0c79666
prabathmadushan5@gmail.com <prabathmadushan5@gmail.com>
2021-05-02 15:03:13 +0530

    header

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Main from './screens/Main';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bp3-dark">
       <Main/>
     </div>
   );
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,22 @@
-import './styles.scss'
+import {
+  Alignment,
+  Navbar,
+  NavbarGroup,
+  NavbarHeading,
+} from "@blueprintjs/core";
+import React from "react";
+import "./styles.scss";
 
-const SideNave = () =>{
-    return <div>
-       
+const SideNave = () => {
+  return (
+    <div>
+      <Navbar>
+        <NavbarGroup align={Alignment.LEFT}>
+          <NavbarHeading>Desk Demon</NavbarHeading>
+        </NavbarGroup>
+      </Navbar>
     </div>
-}
+  );
+};
 
 export default SideNave;
