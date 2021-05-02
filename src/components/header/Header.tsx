99908283dd57e61f52a3d827ade1df91b3a399b9
prabathmadushan5@gmail.com <prabathmadushan5@gmail.com>
2021-05-02 16:11:55 +0530

    avator

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,21 @@
-import {
-  Alignment,
-  Navbar,
-  NavbarGroup,
-} from "@blueprintjs/core";
+import { Alignment, Navbar, NavbarGroup } from "@blueprintjs/core";
 import React from "react";
-import {SiNodemon} from "react-icons/si"
+import { SiNodemon } from "react-icons/si";
 import "./styles.scss";
 
 const Header = () => {
   return (
     <div>
-      <Navbar style={{height:"70px"}}>
-        <NavbarGroup align={Alignment.LEFT} style={{height:"100%"}}>
-          <h2><SiNodemon/> Desk Demon</h2>
+      <Navbar style={{ height: "70px" }}>
+        <NavbarGroup align={Alignment.LEFT} style={{ height: "100%" }}>
+          <h2>
+            <SiNodemon /> Desk Demon
+          </h2>
         </NavbarGroup>
+        <Navbar.Group align={Alignment.RIGHT} style={{ height: "70px" }}>
+          <label className="display-name">Geeth Sandaru</label>
+          <img src="https://scontent.fcmb4-1.fna.fbcdn.net/v/t1.6435-9/143898506_3685239928233146_4483467510135757867_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=o36Fs7nORR8AX-MCve4&_nc_ht=scontent.fcmb4-1.fna&oh=c49138393a8c63f33239183d940ae6a6&oe=60B4F2AB" className="avator-image" alt="user avator" />
+        </Navbar.Group>
       </Navbar>
     </div>
   );
--- src/components/side-nav/SideNav.tsx
+++ src/components/side-nav/SideNav.tsx
@@ -2,19 +2,25 @@ import './styles.scss'
 import {FaMagic,FaShoppingCart} from 'react-icons/fa'
 import {AiOutlineAreaChart} from 'react-icons/ai'
 import {GiClothes} from 'react-icons/gi'
-import { useHistory } from 'react-router'
+import { Route, useHistory, useLocation } from 'react-router'
 import { ROUTES } from '../../screens/Main'
+import { useEffect } from 'react'
 
 const SideNav = () =>{
 
     const {push} = useHistory();
+    const {pathname} = useLocation();
+
+    useEffect(()=>{
+        console.log(pathname)
+    },[pathname])
 
     return <div className="side-nav">
        <ul>
-           <li onClick={()=>push(ROUTES.HOME)} ><AiOutlineAreaChart/>Dashboard</li>
-           <li onClick={()=>push(ROUTES.SALE_PREDICTION)}><FaMagic/>Sales Prediction</li>
-           <li onClick={()=>push(ROUTES.CLOTHING)}><GiClothes/>Clothing</li>
-           <li onClick={()=>push(ROUTES.ORDERS)}><FaShoppingCart/>Orders</li>
+           <li className={pathname===ROUTES.HOME?"active":""} onClick={()=>push(ROUTES.HOME)} ><AiOutlineAreaChart/>Dashboard</li>
+           <li className={pathname===ROUTES.SALE_PREDICTION?"active":""}onClick={()=>push(ROUTES.SALE_PREDICTION)}><FaMagic/>Sales Prediction</li>
+           <li className={pathname===ROUTES.CLOTHING?"active":""}onClick={()=>push(ROUTES.CLOTHING)}><GiClothes/>Clothing</li>
+           <li className={pathname===ROUTES.ORDERS?"active":""}onClick={()=>push(ROUTES.ORDERS)}><FaShoppingCart/>Orders</li>
        </ul>
     </div>
 }
