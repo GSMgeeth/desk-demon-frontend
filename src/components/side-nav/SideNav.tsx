e8fd3721fbe37b6ccd586045c695ccb7095d8329
prabathmadushan5@gmail.com <prabathmadushan5@gmail.com>
2021-05-02 15:38:50 +0530

    side nav

--- src/components/side-nav/SideNav.tsx
+++ src/components/side-nav/SideNav.tsx
@@ -1,8 +1,16 @@
 import './styles.scss'
+import {FaMagic,FaShoppingCart} from 'react-icons/fa'
+import {AiOutlineAreaChart} from 'react-icons/ai'
+import {GiClothes} from 'react-icons/gi'
 
 const SideNav = () =>{
     return <div className="side-nav">
-       
+       <ul>
+           <li><AiOutlineAreaChart/>Dashboard</li>
+           <li><FaMagic/>Sales Prediction</li>
+           <li><GiClothes/>Clothing</li>
+           <li><FaShoppingCart/>Orders</li>
+       </ul>
     </div>
 }
 
